b07505297eee134f3ad34af303590cac0cf999f5
Mezbah15 <mezba0044@gmail.com>
2026-06-20 20:56:41 +0600

    Initial commit

---
+++ SmartITSolution-Invoice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartITSolution_Invoice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
