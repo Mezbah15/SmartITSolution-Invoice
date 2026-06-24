767831c3c043b7dccd7d777c7a78ea7a040400cb
Mezbah15 <mezba0044@gmail.com>
2026-06-24 07:20:14 +0600

    Form Initialize

--- SmartITSolution-Invoice/Controllers/HomeController.cs
+++ SmartITSolution-Invoice/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SmartITSolution_Invoice.Models;
 
 namespace SmartITSolution_Invoice.Controllers
@@ -17,7 +18,32 @@ namespace SmartITSolution_Invoice.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult Invoice()
+        {
+            //var model = new InvoiceViewModel
+            //{
+            //    Participant = participant
+            //};
 
+            //var imageUrl = $"https://storage.hki-2025.com/ski-images/{participant.Image}";
+            //using (var client = new HttpClient())
+            //{
+            //    var imageBytes = await client.GetByteArrayAsync(imageUrl);
+            //    var base64Image = Convert.ToBase64String(imageBytes);
+            //    model.Base64Image = base64Image;
+            //}
+            //var pdf = new ViewAsPdf("_AdmitPartial", model, null, true)
+            //{
+            //    FileName = $"{model.Participant.InvoiceNumber}.pdf",
+            //    PageSize = Rotativa.AspNetCore.Options.Size.A4,
+            //    PageMargins = new Rotativa.AspNetCore.Options.Margins(10, 10, 10, 10),
+            //    CustomSwitches = "--encoding utf-8 --disable-smart-shrinking --load-media-error-handling ignore --load-error-handling ignore"
+            //};
+
+            //return pdf;
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ SmartITSolution-Invoice/Models/InvoiceViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace SmartITSolution_Invoice.Models
+{
+    public class InvoiceViewModel
+    {
+        public string InvoiceNo { get; set; } = string.Empty;
+
+        public DateTime Date { get; set; }
+
+        public string CustomerDetails { get; set; } = string.Empty;
+        public string Note { get; set; } = string.Empty;
+        public string InWord { get; set; } = string.Empty;
+
+        public List<InvoiceItemViewModel> Items { get; set; } = new();
+    }
+    public class InvoiceItemViewModel
+    {
+        public string Description { get; set; } = string.Empty;
+
+        public decimal Quantity { get; set; }
+
+        public decimal Rate { get; set; }
+
+        public decimal Amount => Quantity * Rate;
+    }
+}
